c6ac77aa459d24eca58487364cd4e77dc75da6ad
Raphaël Nussbaumer <rafnuss@gmail.com>
2021-04-09 21:55:54 +0200

    Rename the bird race, add counters, change table and sidebar

--- the-bird-race/script.js
+++ the-bird-race/script.js
@@ -1,15 +1,20 @@
 window.onload = function () {
 
     // MAP
-    map = L.map('mapid');
+    map = L.map('mapid2');
     L.tileLayer.provider('Esri.WorldImagery').addTo(map);
     checklist_marker = L.featureGroup().addTo(map);
 
     jQuery.getJSON('checklist_list.json', function(checklist_list){
-        console.log(checklist_list)
+
+        var icon = L.AwesomeMarkers.icon({
+            icon: 'coffee',
+            iconColor: '#006382'
+          });
+
         checklist_list.forEach(e => {
             console.log(e.Latitude)
-            L.marker([e.Latitude, e.Longitude])
+            L.marker([e.Latitude, e.Longitude], {icon: icon})
             .bindPopup('<a href="https://ebird.org/profile/'+e.user_id+'">'+e.user_name+'</a><br>'+
             '<a href="https://ebird.org/checklist/'+e.user_subId+'">'+e.Location+'</a><br>'+
             'Number of Species: '+e.nb_species)
@@ -18,17 +23,25 @@ window.onload = function () {
         map.fitBounds(checklist_marker.getBounds());
     })
 
-
-    jQuery.getJSON('species_list_sp_only.json', function(species_list){
-        jQuery('#species-nb').html(species_list.length)
+    // COUNTER
+    var counter = [214,12,23,16] // species, observers, sightings, hours
+    jQuery('.counter').each(function(i){
+      var $this = $(this);
+      $this.text(counter[i])
+      jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.ceil(this.Counter));
+        }
+      });
     })
+    
 
 
     // Ranking
     var $table = $('#table');
     jQuery.getJSON('user_list.json', function(user_list){
-        console.log(user_list)
-        jQuery('#users-nb').html(user_list.length)
         $('#table').bootstrapTable({
             data: user_list.map( x => { 
                 x.user_name = '<a href="https://ebird.org/profile/'+x.user_id+'">'+x.user_name+'</a>';
